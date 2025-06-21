36a81d64be0b6b77f6acdcc6bc0756c26a05032c
julian <js490@hdm-stuttgart.de>
2025-06-21 16:22:42 +0200

    implemented dtos for todo request and response, getters and setters still missing

---
+++ src/main/java/model/TodoRequest.java
@@ -0,0 +1,8 @@
+package model;
+
+public class TodoRequest {
+    private String title;
+    private String description;
+
+    //todo: getters and setters
+}
---
+++ src/main/java/model/TodoResponse.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.time.Instant;
+
+public class TodoResponse {
+    private String id;
+    private String title;
+    private String description;
+    private String status;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    //todo: setters and getters
+}
