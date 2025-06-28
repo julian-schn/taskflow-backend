0ad309b6baf482cdbff4475be540cfddb70489aa
julian <js490@hdm-stuttgart.de>
2025-06-28 22:38:02 +0200

    pivoting to use h2 for test locally, and dynamodb in prod

--- src/main/java/config/DynamoDBConfig.java
+++ src/main/java/config/DynamoDBConfig.java
@@ -1,5 +1,6 @@
 package config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
@@ -8,12 +9,13 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import java.net.URI;
 
 @Configuration
+@ConditionalOnProperty(name = "dynamodb.enabled", havingValue = "true", matchIfMissing = false)
 public class DynamoDBConfig {
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .endpointOverride(URI.create("http://localhost:8000"))
+                .endpointOverride(URI.create("http://dynamodb-local:8000"))
                 .region(Region.of("eu-central-1"))
                 .build();
     }
